90eaea9c53c0c5ad24651e8fbb0d6b703d9d2e68
Pranshu Aggarwal <40149082+pranshuag9@users.noreply.github.com>
2021-09-19 17:18:02 +0530

    Added concept of SPREAD and REST operators in JS

---
+++ 06Advancish/05SpreadAndRESTOperators.js
@@ -0,0 +1,28 @@
+// Spread Operator and REST Operator is ...args depending upon the condition.
+
+var returnedValue = Math.max(2, 5, 7, 4, 2, 8);
+console.log(returnedValue);
+
+var myObj = {}
+
+Object.assign(myObj, {a: 1, b: 2, c: 3}, {z: 9, y: 8, x: 7});
+console.log(myObj);
+
+
+function sumOne(a, b) {
+	return a + b;
+}
+
+var myArr = [5, 4];
+
+// console.log(sumOne(...myArr)); // This is SPREAD operator.
+
+// REST Operator used inside function paramter.
+function sumTwo(a, b, ...args) {
+	let result = 0;
+	result = a * b;
+	// for(const i of args) result += i;
+	args.forEach((i) => result += i);
+	return result;
+}
+console.log(sumTwo(1, 2, 3, 4, 5));
